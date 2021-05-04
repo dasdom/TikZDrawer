7004378cadf465e99ddf86e74a06359cd0c4c567
Dominik Hauser <dom@dasdom.de>
2021-05-04 09:30:01 +0200

    Initial Commit

---
+++ TikZDrawer/ContentView.swift
@@ -0,0 +1,18 @@
+//  Created by Dominik Hauser on 04/05/2021.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TikZDrawer/TikZDrawerApp.swift
@@ -0,0 +1,14 @@
+//  Created by Dominik Hauser on 04/05/2021.
+//  
+//
+
+import SwiftUI
+
+@main
+struct TikZDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
